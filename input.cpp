6ff36acb11bcc4e1be214e83de403e65695356bb
HridaySarder <hridoyaug12@gmail.com>
2024-05-19 09:44:09 +0600

    first commit

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // float a;
+    // char b;
+    // cin >> a >> b;
+    // cout << int(a) << endl
+    //      << int(b);
+    // return 0;
+    char name;
+    int age;
+    cin >> name >> age;
+    cout << "Name: " << name << endl
+         << "Age: " << age;
+}
---
+++ output.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main()
+{
+    int a=10,b=20;
+    std::cout<<a<<std::endl<<b;
+    return 0;
+}
