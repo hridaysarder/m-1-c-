568803110ff70849852d4cb6319bb8ab22114c1f
HridaySarder <hridoyaug12@gmail.com>
2024-05-19 20:03:36 +0600

    added array string

---
+++ array_string.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+// #include <string>
+using namespace std;
+int main()
+{
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << a[i] << endl;
+    // }
+
+    int a;
+    cin >> a;
+    getchar();
+    char s[100];
+    cin.getline(s, 100);
+    cout << a << endl;
+    cout << s << endl;
+}
